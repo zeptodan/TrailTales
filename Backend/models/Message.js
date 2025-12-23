b92ee61867f0aa40c7ed7425064f1f6e95e6886d
Muhammad Anas <muhammadannas138@gmail.com>
2025-12-23 10:41:58 +0500

    added Message schema

--- Backend/models/Message.js
+++ Backend/models/Message.js
@@ -1,24 +1,37 @@
 import mongoose from "mongoose";
 
-const MessageSchema = new mongoose.Schema({
+/*
+  Message Schema
+  Represents a private message exchanged between two users
+*/
+const MessageSchema = new mongoose.Schema(
+  {
     sender: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",        // Reference to the user who sent the message
+      required: true,
     },
-    receiver: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true
-    },
-    content: {
-        type: String,
-        required: true
-    },
-    read: {
-        type: Boolean,
-        default: false
-    }
-}, { timestamps: true });
 
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",        // Reference to the user who receives the message
+      required: true,
+    },
+
+    content: {
+      type: String,
+      required: true,     // Actual message text
+    },
+
+    read: {
+      type: Boolean,
+      default: false,     // Indicates whether the message has been read
+    },
+  },
+  {
+    timestamps: true,     // Automatically adds createdAt & updatedAt fields
+  }
+);
+
+// Export Message model
 export default mongoose.model("Message", MessageSchema);
